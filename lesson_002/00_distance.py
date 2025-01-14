abdcf8494dfb82f4c859638b79c8b74af95853ef
Artem <artemtomko03@gmail.com>
2025-01-14 16:42:12 +0100

    Патч. Я додав коментарі та довів до розуму гарний вивід значень

--- lesson_002/00_distance.py
+++ lesson_002/00_distance.py
@@ -17,11 +17,15 @@ sites = {
 
 distances = {}
 
+# Запит у користувача міста від якого потрібно вирахувати відстань
+
 DataUserKeys = str(input('Введіть назву міста відстань до якого '
                          f'ви хочете дізнатися (доступно {list(sites.keys())})'
                          f'\nВвод: '))
 
 Coordinates = []
+
+# Перевірка чи є в словарю цей ключ(місто)
 
 try:
     sites[DataUserKeys]
@@ -30,18 +34,24 @@ except KeyError:
           ' опечатались або в нас не має цього міста.')
     print(f'Ви ввели: {DataUserKeys}\nДоступні у нас {list(sites.keys())}')
 else:
+    # Якщо є, то виповняються наступні команди
     for i in sites.keys():
+        # В список Coordinates та UserCoordinates записуються данні по ключам
         Coordinates = sites[i]
         UserCoordinates = sites[DataUserKeys]
+        # Переписуються потрібні данні в потрібні перемінні для мат. операцій
         x1 = UserCoordinates[0]
         y1 = UserCoordinates[1]
         x2 = Coordinates[0]
         y2 = Coordinates[1]
-        res = str(round(math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2), 1)) + ' Метрів'
+        # В перемінну записується математична операція. Після чого з потрібним ключем записали потрібні данні.
+        res = round(math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2), 1)
         distances.setdefault(i, res)
+    # Видалення безкорисних результатів
     distances.pop(DataUserKeys)
+    # Спроба гарно вивести значення
     for item in distances.items():
-        print(item)
+        print(f'Від {DataUserKeys} До {item[0]}: {item[1]} Кілометрів')
 
 
 
