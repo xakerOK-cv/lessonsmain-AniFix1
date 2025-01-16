2d20935327b3c11c0e3d4a5bd5724d365139ba8d
Artem <artemtomko03@gmail.com>
2025-01-16 15:57:28 +0100

    Нове завдання.

--- lesson_002/07_secret.py
+++ lesson_002/07_secret.py
@@ -21,4 +21,30 @@ secret_message = [
 # Требуется задать конкретные индексы, например secret_message[3][12:23:4]
 # Если нужны вычисления и разные пробы - делайте это в консоли пайтона, тут нужен только результат
 
-# TODO вывести расшифрованное сообщение
+def decoding(ciphers, keys):
+    res = ''
+    for index, cipher in enumerate(ciphers) :
+        fake_res = None
+        if isinstance(keys[index], list) :
+            try:
+                if keys[index][2] == 2 :
+                    res = res + cipher[slice(keys[index][0] - 1, keys[index][1], keys[index][-1])] + ' '
+                elif keys[index][2] == -1 :
+                    fake_res = cipher[slice(keys[index][0] - 1, keys[index][1])]
+                    res = res + fake_res[::-1] + ' '
+            except IndexError :
+                res = res + cipher[slice(keys[index][0] - 1, keys[index][1])] + ' '
+        else:
+            res = res + cipher[keys[0] - 1] + ' '
+    return res[slice(0, -1)]
+
+
+def main():
+    keys = {0 : 4, 1 : [10, 13], 2 : [6, 15, 2],
+            3 : [8, 13, -1], 4 : [17, 21, -1]
+            }
+    deciphering = decoding(secret_message, keys)
+    print(deciphering)
+
+if __name__ == '__main__' :
+    main()
