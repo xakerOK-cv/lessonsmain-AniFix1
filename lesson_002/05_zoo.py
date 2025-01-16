068cb91773e50c53a5f9d523f6ad37991fca6b79
Artem <artemtomko03@gmail.com>
2025-01-16 13:17:09 +0100

    Нове завдання. Є коментар.

--- lesson_002/05_zoo.py
+++ lesson_002/05_zoo.py
@@ -7,19 +7,62 @@ zoo = ['lion', 'kangaroo', 'elephant', 'monkey', ]
 
 # посадите медведя (bear) между львом и кенгуру
 #  и выведите список на консоль
-# TODO здесь ваш код
+def write(data):
+    zoo.insert(1, data)
+    return None
 
 # добавьте птиц из списка birds в последние клетки зоопарка
 birds = ['rooster', 'ostrich', 'lark', ]
 #  и выведите список на консоль
-# TODO здесь ваш код
+def  extension(data):
+    zoo.extend(data)
+    return None
 
-# уберите слона
+# уберите слона (elephant)
 #  и выведите список на консоль
-# TODO здесь ваш код
+def remove(data):
+    zoo.remove(data)
+    return None
+
 
 # выведите на консоль в какой клетке сидит лев (lion) и жаворонок (lark).
 # Номера при выводе должны быть понятны простому человеку, не программисту.
-# TODO здесь ваш код
+def cage(data):
+    res = zoo.index(data)
+    return res
+
+def out():
+    res = ''
+    for index, x in enumerate(zoo):
+        if index == len(zoo)-1:
+            res = res + zoo[index] + '.'
+        else:
+            res = res + zoo[index] + ', '
+    return res
+
+def main():
+    # Перший варіант кода. Але мені не сподобалося, як воно виводить списки(не гарно).
+
+    # write(data='bear')
+    # print(zoo)
+    # extension(data=birds)
+    # print(zoo)
+    # remove(data='elephant')
+    # print(zoo)
+    # print(f'Лев сидить в {cage('lion') + 1} клітці, а'
+    #       f' жаворонок в {cage('lark') + 1} клітці.')
+
+    write(data='bear')
+    print(out())
+    extension(data=birds)
+    print(out())
+    remove(data='elephant')
+    print(out())
+    print(f'Лев сидить в {cage('lion') + 1} клітці, а'
+          f' жаворонок в {cage('lark') + 1} клітці.')
 
 
+
+
+if __name__ == '__main__' :
+    main()
