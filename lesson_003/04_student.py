c5ece091c7240a69a03f08d3565b86bfc4b5c4be
Artem <artemtomko03@gmail.com>
2025-03-25 17:53:31 +0100

    task04: completed

--- lesson_003/04_student.py
+++ lesson_003/04_student.py
@@ -10,5 +10,31 @@
 #   Студенту надо попросить ХХХ.ХХ рублей
 
 educational_grant, expenses = 10000, 12000
+inflation_rate, period = 3, 10
 
-# TODO здесь ваш код
+
+def price_inflation(funds, inflation, duration):
+    inflation = inflation / 100 + 1
+    duration, res = duration - 1, funds
+    while duration > 0:
+        duration -= 1
+        funds *= inflation
+        res += funds
+    return res
+
+def remaining(funds, total_costs, duration):
+    res = (funds * duration) - total_costs
+    return res
+
+def main():
+    need_funds = price_inflation(expenses, inflation_rate, period)
+    arrears = round(remaining(educational_grant, need_funds, period), 2)
+    if arrears >= 0:
+        arrears = 0
+    else:
+        arrears = abs(arrears)
+    print(f'Студенту потрібно попросити {arrears} рублів')
+    return
+
+if __name__ == '__main__':
+    main()
