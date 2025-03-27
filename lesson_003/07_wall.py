49eb2e168e7e7d026c253035d3142c56329bac61
Artem <artemtomko03@gmail.com>
2025-03-27 14:54:55 +0100

    task07: completed

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -1,11 +1,35 @@
 # -*- coding: utf-8 -*-
 
 # (цикл for)
-import simple_draw
+import simple_draw as sd
 
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 
-# TODO здесь ваш код
+def bricks(length, width):
+    for row in range(0, 601, int(length/2)):
+        for brick in range(0, 601, width*2):
+            if not row % 100 == 0:
+                brick -= width
+            points = [sd.Point(row + (length / 2), brick),
+                     sd.Point(row + (length / 2), brick+width),
+                     sd.Point(row, brick+width),
+                     sd.Point(row, brick+width*2),
+                     sd.Point(row + (length / 2), brick+width*2)]
+            for i in range(1, 5):
+                sd.line(points[i-1], points[i], sd.COLOR_DARK_YELLOW, 2)
 
-simple_draw.pause()
+
+
+
+
+def main():
+    sd.resolution = (600, 600)
+
+    bricks(100, 50)
+    sd.pause()
+    return
+
+
+if __name__ == '__main__':
+    main()
