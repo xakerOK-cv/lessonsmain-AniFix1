0f76d73fc6209caea05f30eecaaf0eb311671319
Artem <artemtomko03@gmail.com>
2025-06-13 12:44:41 +0200

    4.4 ЗАВЕРШЕНО

--- lesson_004/04_fractal.py
+++ lesson_004/04_fractal.py
@@ -9,11 +9,27 @@ import simple_draw as sd
 # - длина ветвей,
 # Отклонение ветвей от угла рисования принять 30 градусов,
 
+# def draw_branches(start_point, angle, length):
+#     start_point = sd.vector(start_point, angle, length)
+#     angle += 30
+#     sd.vector(start_point, angle, length)
+
 # 2) Сделать draw_branches рекурсивной
 # - добавить проверку на длину ветвей, если длина меньше 10 - не рисовать
 # - вызывать саму себя 2 раза из точек-концов нарисованных ветвей,
 #   с параметром "угол рисования" равным углу только что нарисованной ветви,
 #   и параметром "длинна ветвей" в 0.75 меньшей чем длина только что нарисованной ветви
+
+# def draw_branches(start_point, angle, length):
+#     if length < 3 :
+#         return None
+#     start_point = sd.vector(start_point, angle, length)
+#     angle_p_n = angle + 30, angle - 30
+#     length *= 0.75
+#     for _angle in angle_p_n:
+#         draw_branches(start_point, _angle, length)
+
+
 
 # 3) первоначальный вызов:
 # root_point = get_point(300, 30)
@@ -26,7 +42,6 @@ import simple_draw as sd
 
 # можно поиграть -шрифтами- цветами и углами отклонения
 
-# TODO здесь ваш код
 
 # 4) Усложненное задание (делать по желанию)
 # - сделать рандомное отклонение угла ветвей в пределах 40% от 30-ти градусов
@@ -36,6 +51,28 @@ import simple_draw as sd
 # Пригодятся функции
 # sd.random_number()
 
-sd.pause()
+def draw_branches(start_point, angle, length):
+    if length < 4 :
+        return None
+    start_point = sd.vector(start_point, angle, length)
+
+    # Angle Positive and Negative
+    angle_p_n = ( angle + (30 + (sd.random_number(-40, 40) / 100)),
+                  angle - (30 + (sd.random_number(-40, 40) / 100)) )
+    length *= (0.75 + (sd.random_number(-20, 20) / 100))
+
+    for _angle in angle_p_n:
+        draw_branches(start_point, _angle, length)
+
+def main():
+    sd.resolution = (700, 750)
+
+    root_point = sd.Point(350, 30)
+    draw_branches(start_point=root_point, angle=90, length=100)
 
 
+    sd.pause()
+
+
+if __name__ == '__main__':
+    main()
