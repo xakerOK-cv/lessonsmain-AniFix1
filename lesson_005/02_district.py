c9b374ca5963775812851d0273befdb7e32fcf5f
Artem <artemtomko03@gmail.com>
2025-08-17 17:20:16 +0200

    5.2 готово

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
+import room_1
+import room_2
 
 # Составить список всех живущих на районе и Вывести на консоль через запятую
 # Формат вывода: На районе живут ...
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
-# TODO здесь ваш код
+rayon = ', '.join([*room_1.folks, *room_2.folks], )
 
-
-
-
+print(f'На районі живуть: {rayon}')
