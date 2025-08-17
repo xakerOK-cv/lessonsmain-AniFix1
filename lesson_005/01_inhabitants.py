33c31a56c9f9c15289a0170efe0fd4ce2bd71990
Artem <artemtomko03@gmail.com>
2025-08-17 17:08:26 +0200

    5.1 готово

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
+import room_1
+import room_2
 
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: В комнате room_1 живут: ...
 
-# TODO здесь ваш код
+print(f'В кімнаті room_1 живуть: {room_1.folks}',
+      f'\nВ кімнаті roon_2 живуть: {room_2.folks}')
 
 
