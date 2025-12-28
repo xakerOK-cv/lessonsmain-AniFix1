911c9c407ec2e5b735d4b41d8d9b0df62519dd7a
Artem <artemtomko03@gmail.com>
2025-12-28 23:57:31 +0100

    6.1 зробив

---
+++ lesson_006/MasterMindEngine.py
@@ -0,0 +1,21 @@
+import random
+
+
+the_number = ''
+
+def guess_the_number():
+    global the_number
+    the_number = the_number.join(random.sample('123456789', 4))
+
+
+def check_the_number(u_number):
+    lists = [0, 0]
+
+    for index, number in enumerate(u_number):
+        for my_index, my_number in enumerate(the_number):
+            if index == my_index and number == my_number:
+                lists[1] += 1
+            elif number == my_number:
+                lists[0] += 1
+
+    return lists
